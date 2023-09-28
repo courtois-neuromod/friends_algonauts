16941284ad80a88e8241cab2e06ebdc6ed9968b8
MarieStLaurent <marie.stl@gmail.com>
2023-09-28 17:42:14 -0400

    [DATALAD] Recorded changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='preprocessing the friends dataset for the Algonauts 2025 competition',
+    author='Marie St-Laurent',
+    license='MIT',
+)
